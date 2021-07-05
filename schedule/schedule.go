235f81582d4fd9381db434e787c1db70c513429f
lihaonihao <568330385@qq.com>
2021-07-05 11:14:59 +0800

    增加限流

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"mid-server/register"
 	"mid-server/task"
-	"mid-server/work"
 	"net/http"
 	"sync"
 )
@@ -21,29 +20,49 @@ type checkResult struct {
 }
 
 type schedule struct {
-	resultChan chan checkResult
-	register   register.Register
-	workers    chan work.Worker
+	resultChan   chan checkResult
+	register     register.Register
+	maxGoRoutine int
+	//限流
+	goRoutineChan chan struct{}
 }
 
-func NewSchedule(maxRoutine int, r register.Register) *schedule {
-	if maxRoutine < MIN_VALID_ROUTINE || maxRoutine > MAX_VALID_ROUTINE {
+func NewSchedule(maxGoRoutine int, r register.Register) *schedule {
+	if maxGoRoutine < MIN_VALID_ROUTINE || maxGoRoutine > MAX_VALID_ROUTINE {
 		return nil
 	}
 	s := &schedule{}
 	s.resultChan = make(chan checkResult)
 	s.register = r
-	s.workers = make(chan work.Worker, maxRoutine)
+	s.maxGoRoutine = maxGoRoutine
+	s.goRoutineChan = make(chan struct{}, maxGoRoutine)
+	s.initGoroutineChan()
 	return s
 }
 
-func (s *schedule) Execute(ctx context.Context, subTasks []task.Task) ([]task.Result, error) {
+func (s schedule) initGoroutineChan() {
+	nowGoRoutine := len(s.goRoutineChan)
+	for i := nowGoRoutine; i < s.maxGoRoutine; i++ {
+		s.addGoRoutine()
+	}
+}
+
+func (s schedule) waitGoRoutine() {
+	<-s.goRoutineChan
+}
+
+func (s schedule) addGoRoutine() {
+	s.goRoutineChan <- struct{}{}
+}
+
+func (s schedule) Execute(ctx context.Context, subTasks []task.Task) ([]task.Result, error) {
 	results := make([]task.Result, 0, len(subTasks))
 	group := sync.WaitGroup{}
 	checkoutChan := make(chan struct{})
 	resultChan := make(chan checkResult)
+	group.Add(len(subTasks))
 	for _, subTask := range subTasks {
-		group.Add(1)
+		s.waitGoRoutine()
 		go s.check(subTask, resultChan)
 	}
 	go func() {
@@ -54,16 +73,18 @@ func (s *schedule) Execute(ctx context.Context, subTasks []task.Task) ([]task.Re
 		select {
 		case result := <-resultChan:
 			results = append(results, *result.Result)
+			s.addGoRoutine()
 			group.Done()
 		case <-checkoutChan:
 			return results, nil
 		case <-ctx.Done():
+			s.initGoroutineChan()
 			return results, errors.New("time out")
 		}
 	}
 }
 
-func (s *schedule) check(subTask task.Task, resultChan chan checkResult) {
+func (s schedule) check(subTask task.Task, resultChan chan checkResult) {
 	t := s.register.Tasker(context.Background(), subTask.TaskType)
 	if t == nil {
 		resultChan <- checkResult{
@@ -79,7 +100,7 @@ func (s *schedule) check(subTask task.Task, resultChan chan checkResult) {
 	}
 }
 
-func (s *schedule) checkByTemplate(task *task.Task, tasker task.Tasker) (*task.Result, error) {
+func (s schedule) checkByTemplate(task *task.Task, tasker task.Tasker) (*task.Result, error) {
 	subResult, checkErr := tasker.Check(task)
 	if checkErr != nil {
 		return s.errorResult(task, checkErr.Error()), checkErr
@@ -87,13 +108,13 @@ func (s *schedule) checkByTemplate(task *task.Task, tasker task.Tasker) (*task.R
 	return s.successResult(task, subResult), nil
 }
 
-func (s *schedule) errorResult(subTask *task.Task, err string) *task.Result {
+func (s schedule) errorResult(subTask *task.Task, err string) *task.Result {
 	result := task.NewResult(subTask)
 	result.Error = err
 	return result
 }
 
-func (s *schedule) successResult(subTask *task.Task, subResult string) *task.Result {
+func (s schedule) successResult(subTask *task.Task, subResult string) *task.Result {
 	result := task.NewResult(subTask)
 	result.SubResultCode = http.StatusOK
 	result.SubResult = subResult
